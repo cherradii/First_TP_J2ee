6f3637f35f228b12bc25690c2906c6acf603db5d
cherradi <cherradi@DESKTOP-LQULD2S>
2021-02-08 16:08:03 +0100

    add login form

--- GestionDesAnnonces/src/main/java/com/cherradi/ensah/web/UserController.java
+++ GestionDesAnnonces/src/main/java/com/cherradi/ensah/web/UserController.java
@@ -20,7 +20,28 @@ public class UserController extends HttpServlet{
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String action = req.getServletPath();
-		System.out.println(action);
+		switch (action) {
+			case "/motDePasseOublie":
+				recuperPswd(req, resp);
+				break;
+			default:
+				login(req, resp);
+				break;
+		}
+	}
+
+	private void login(HttpServletRequest req, HttpServletResponse resp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void recuperPswd(HttpServletRequest req, HttpServletResponse resp) {
+		try {
+			this.getServletContext().getRequestDispatcher("/views/login.jsp").forward(req, resp);
+		} catch (ServletException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
